f3ae385c12e2bd5b04651b610c513d743b0db471
Jakub Falkowski <jakub.falkowski@student.put.poznan.pl>
2019-01-01 12:00:00 +0000

    Initial commit

---
+++ example.java
@@ -0,0 +1,50 @@
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
---
+++ example.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>My first PHP page</h1>
+<?php
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+?> 
+</body>
+</html>
